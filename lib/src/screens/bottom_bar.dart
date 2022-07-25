494695a518ca3e54542e44ff83cf8cf521fc3aad
Heru Handika <herubiolog@gmail.com>
2022-07-25 18:44:41 -0500

    Remove unused imports.

--- lib/src/screens/bottom_bar.dart
+++ lib/src/screens/bottom_bar.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
