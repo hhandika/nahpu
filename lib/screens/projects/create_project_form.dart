de084225b9eae83bda35b30a9e69bcb7d6d500ca
Heru Handika <herubiolog@gmail.com>
2022-07-30 14:52:38 -0500

    Add form validation and fix issues of accessing multiple database.

--- lib/screens/projects/create_project_form.dart
+++ lib/screens/projects/create_project_form.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:drift/drift.dart' as db;
 import 'package:uuid/uuid.dart';
+import 'package:provider/provider.dart';
 
 import '../projects/project_menu.dart';
 import 'package:nahpu/database/database.dart';
@@ -14,7 +16,8 @@ class CreateProjectForm extends StatefulWidget {
 }
 
 class _NewProjectFormState extends State<CreateProjectForm> {
-  final _formKey = const Uuid().v4();
+  final _formKey = GlobalKey<FormState>();
+  final _uuidKey = const Uuid().v4();
   final projectNameController = TextEditingController();
   final descriptionController = TextEditingController();
   final collectorController = TextEditingController();
@@ -29,101 +32,131 @@ class _NewProjectFormState extends State<CreateProjectForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create a new project'),
-        backgroundColor: const Color(0xFF2457C5),
-      ),
-      body: Column(children: [
-        Expanded(
-            child: Column(
-          children: [
-            TextFormField(
-              controller: projectNameController,
-              decoration: const InputDecoration(
-                labelText: 'Project name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextFormField(
-              controller: descriptionController,
-              decoration: const InputDecoration(
-                labelText: 'Project description',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextFormField(
-              controller: collectorController,
-              decoration: const InputDecoration(
-                labelText: 'Collector',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextFormField(
-              controller: collectorEmailController,
-              decoration: const InputDecoration(
-                labelText: 'Collector email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextFormField(
-              controller: catNumController,
-              decoration: const InputDecoration(
-                labelText: 'Catalog number start',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            TextFormField(
-              controller: teamLaederController,
-              decoration: const InputDecoration(
-                labelText: 'Team leader',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextFormField(
-              controller: collectorController,
-              decoration: const InputDecoration(
-                labelText: 'Collector name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            Wrap(spacing: 10, children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.grey),
-                ),
-                child: const Text('Cancel'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(const Color(0xFF2457C5)),
-                ),
-                child: const Text('Create'),
-                onPressed: () {
-                  _createProject();
+        appBar: AppBar(
+          title: const Text('Create a new project'),
+          backgroundColor: const Color(0xFF2457C5),
+        ),
+        body: Center(
+            child: Form(
+          key: _formKey,
+          child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(
+                    child: ConstrainedBox(
+                        constraints: const BoxConstraints(maxWidth: 400),
+                        child: Column(
+                          children: [
+                            ProjectFormField(
+                              controller: projectNameController,
+                              labelText: 'Project name*',
+                              inputFormatters: [
+                                LengthLimitingTextInputFormatter(50),
+                                FilteringTextInputFormatter.allow(
+                                  RegExp(r'[a-zA-Z0-9-_]+|\s'),
+                                ),
+                              ],
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Project name is required';
+                                }
+                                return null;
+                              },
+                            ),
+                            ProjectFormField(
+                              controller: descriptionController,
+                              labelText: 'Project description*',
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Project description is required';
+                                }
+                                return null;
+                              },
+                            ),
+                            ProjectFormField(
+                              controller: collectorController,
+                              labelText: 'Collector*',
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Collector name is required';
+                                }
+                                return null;
+                              },
+                            ),
+                            ProjectFormField(
+                              controller: collectorEmailController,
+                              labelText: 'Collector email*',
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Collector email is required';
+                                }
+                                return null;
+                              },
+                            ),
+                            ProjectFormField(
+                              controller: catNumController,
+                              labelText: 'Catalog number start*',
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.allow(
+                                  RegExp(r'[0-9]+'),
+                                ),
+                              ],
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Catalog number start is required';
+                                } else if (!value.isValidCatNum) {
+                                  return 'Catalog number must be a number';
+                                }
+                                return null;
+                              },
+                            ),
+                            ProjectFormField(
+                              controller: teamLaederController,
+                              labelText: 'Team leader',
+                            ),
+                            Wrap(spacing: 10, children: [
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.grey),
+                                ),
+                                child: const Text('Cancel'),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      const Color(0xFF2457C5)),
+                                ),
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    _createProject();
 
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ProjectMenu()),
-                  );
-                },
-              )
-            ])
-          ],
-        ))
-      ]),
-    );
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const ProjectMenu()),
+                                    );
+                                  }
+                                },
+                                child: const Text('Create'),
+                              )
+                            ])
+                          ],
+                        )))
+              ]),
+        )));
   }
 
   Future<void> _createProject() async {
-    final database = Database();
+    final database = Provider.of<Database>(context, listen: false);
     database.createProject(ProjectCompanion(
-      projectId: db.Value(_formKey),
+      projectId: db.Value(_uuidKey),
       projectName: db.Value(projectNameController.text),
       projectDescription: db.Value(descriptionController.text),
       collector: db.Value(collectorController.text),
@@ -131,5 +164,49 @@ class _NewProjectFormState extends State<CreateProjectForm> {
       catNumStart: db.Value(int.parse(catNumController.text)),
       teamLeader: db.Value(teamLaederController.text),
     ));
+  }
+}
+
+class ProjectFormField extends StatelessWidget {
+  const ProjectFormField(
+      {Key? key,
+      required this.labelText,
+      required this.controller,
+      this.keyboardType,
+      this.inputFormatters,
+      this.validator})
+      : super(key: key);
+
+  final String labelText;
+  final TextEditingController controller;
+  final TextInputType? keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
+  final String? Function(String?)? validator;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            hintText: labelText,
+          ),
+          keyboardType: keyboardType,
+          inputFormatters: inputFormatters,
+          validator: validator,
+        ));
+  }
+}
+
+extension StringValidator on String {
+  bool get isValidCatNum {
+    final catNumRegex = RegExp(r'[0-9]');
+    return catNumRegex.hasMatch(this);
+  }
+
+  bool get isValidEmail {
+    final emailRegex =
+        RegExp(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
+    return emailRegex.hasMatch(this);
   }
 }
