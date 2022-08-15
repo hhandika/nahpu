3c168927cc26b117592f2ba562bd8809c7ae89e1
Heru Handika <herubiolog@gmail.com>
2022-08-15 15:38:51 -0500

    Remove commented out code.

--- lib/providers/project.dart
+++ lib/providers/project.dart
@@ -14,32 +14,3 @@ final projectInfoProvider =
   final projectInfo = ref.watch(databaseProvider).getProjectByUuid(uuid);
   return projectInfo;
 });
-
-
-
-
-// final projectListNotifier = StreamProvider<List<ListProjectResult>>((ref) => ref
-//     .watch(databaseProvider)
-//     .getProjectList()
-//     .asStream()
-//     .map((event) => event.reversed.toList()));
-
-// class ProjectListNotifier extends StateNotifier<List<ListProjectResult>> {
-
-//   List<ListProjectResult> _projectList = [];
-//   get projectList => _projectList;
-
-//   final Ref ref;
-
-//   void getProjectList() {
-//     ProjectModel(ref).getProjectList().then((value) {
-//       _projectList = value.reversed.toList();
-//       notifyListeners();
-//     });
-//   }
-
-//   void deleteProject(String projectUuid) {
-//     ProjectModel(ref).deleteProject(projectUuid);
-//     notifyListeners();
-//   }
-// }
