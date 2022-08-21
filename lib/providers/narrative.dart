d8b4e15803dfa5b75e97b36303a20321fcf8b133
Heru Handika <herubiolog@gmail.com>
2022-08-21 11:38:04 -0500

    Remove unused code.

--- lib/providers/narrative.dart
+++ lib/providers/narrative.dart
@@ -10,27 +10,3 @@ final narrativeEntryProvider =
       ref.read(databaseProvider).getAllNarrative(projectUuid);
   return await narrativeEntries;
 });
-
-// final narrativeEntryProvider =
-//     FutureProvider.autoDispose<List<NarrativeData>>((ref) async {
-//   return ref.watch(narrativeProvider.future);
-// });
-
-class NarrativeEntryProvider extends StateNotifier<List<NarrativeData>> {
-  NarrativeEntryProvider() : super([]);
-
-  void addNarrativeEntry(NarrativeData narrativeEntry) {
-    state = [...state, narrativeEntry];
-  }
-
-  void removeNarrativeEntry(NarrativeData narrativeEntry) {
-    state = state.where((element) => element.id != narrativeEntry.id).toList();
-  }
-
-  void updateNarrativeEntry(NarrativeData narrativeEntry) {
-    state = state
-        .map((element) =>
-            element.id == narrativeEntry.id ? narrativeEntry : element)
-        .toList();
-  }
-}
