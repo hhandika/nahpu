57db81b52ab8ac1a0bc76012bd6c250e9f227b1d
Heru Handika <herubiolog@gmail.com>
2022-08-29 16:49:20 -0500

    Implement user setting appearance.

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nahpu/providers/settings.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class AppSettings extends StatefulWidget {
@@ -27,30 +28,49 @@ class MainSettings extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(themeSettingProvider);
+    String themeValue = _getThemeValue(theme);
     return SettingsList(sections: [
       SettingsSection(title: const Text('General'), tiles: [
         SettingsTile.navigation(
           leading: const Icon(Icons.color_lens_rounded),
           title: const Text('Apperance'),
+          value: Text(themeValue),
           onPressed: (context) => Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => const Appearance(),
+              builder: (context) => Appearance(isSelected: themeValue),
             ),
           ),
         ),
       ])
     ]);
   }
+
+  String _getThemeValue(ThemeMode theme) {
+    switch (theme) {
+      case ThemeMode.dark:
+        return 'Dark';
+      case ThemeMode.light:
+        return 'Light';
+      case ThemeMode.system:
+        return 'System';
+    }
+  }
 }
 
-class Appearance extends ConsumerWidget {
-  const Appearance({Key? key}) : super(key: key);
+class Appearance extends ConsumerStatefulWidget {
+  const Appearance({Key? key, required this.isSelected}) : super(key: key);
+  final String isSelected;
+  @override
+  AppearanceState createState() => AppearanceState();
+}
+
+class AppearanceState extends ConsumerState<Appearance> {
+  final List<String> themes = ['dark', 'light', 'system'];
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final List<String> themes = ['dark', 'light', 'system'];
-    List<bool> isSelected = [false, false, false];
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Appearance'),
@@ -63,10 +83,10 @@ class Appearance extends ConsumerWidget {
             (e) {
               return SettingsTile(
                   title: Text(e),
-                  leading: isSelected[themes.indexOf(e)]
-                      ? const Icon(Icons.check)
-                      : null,
+                  trailing:
+                      widget.isSelected == e ? const Icon(Icons.check) : null,
                   onPressed: (_) {
+                    _setTheme(e);
                     Navigator.of(context).pop();
                   });
             },
@@ -74,5 +94,20 @@ class Appearance extends ConsumerWidget {
         )
       ]),
     );
+  }
+
+  void _setTheme(String theme) {
+    final setTheme = ref.read(themeSettingProvider.notifier);
+    switch (theme) {
+      case 'dark':
+        setTheme.setDarkMode();
+        break;
+      case 'light':
+        setTheme.setLightMode();
+        break;
+      case 'system':
+        setTheme.setSystemMode();
+        break;
+    }
   }
 }
