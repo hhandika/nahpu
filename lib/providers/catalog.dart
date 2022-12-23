15748e679760e37c8f3b35169eccc199348dbf85
Heru Handika <herubiolog@gmail.com>
2022-12-23 17:15:27 -0600

    Add catalog number providers.

--- lib/providers/catalog.dart
+++ lib/providers/catalog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nahpu/models/types.dart';
+import 'package:nahpu/providers/settings.dart';
 
 final catalogFmtProvider =
     StateNotifierProvider<CatalogFmtNotifier, CatalogFmt>(
@@ -10,5 +11,39 @@ class CatalogFmtNotifier extends StateNotifier<CatalogFmt> {
 
   void setCatalogFmt(CatalogFmt catalogFmt) {
     state = catalogFmt;
+  }
+}
+
+// We need to save the catalog number to the shared preferences
+// so that we can retrieve it when the app is restarted.
+// and also we can use it to generate the catalog number for the
+// next project.
+final catalogNumberProvider =
+    StateNotifierProvider<CatalogNumberNotifier, int>((ref) {
+  return CatalogNumberNotifier();
+});
+
+class CatalogNumberNotifier extends StateNotifier<int> {
+  CatalogNumberNotifier() : super(0);
+
+  void initCatNum(WidgetRef ref) {
+    final prefs = ref.read(settingProvider);
+    final lastCatNum = prefs.getInt('catNum');
+    if (lastCatNum != null) {
+      state = lastCatNum;
+    }
+  }
+
+  void increaseCatNum() {
+    state++;
+  }
+
+  void decreaseCatNum() {
+    state--;
+  }
+
+  void saveCatNum(WidgetRef ref) {
+    final prefs = ref.read(settingProvider);
+    prefs.setInt('catNum', state);
   }
 }
