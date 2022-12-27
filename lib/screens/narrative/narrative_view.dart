15162179c390013200a473a299985a5d567f51a9
Heru Handika <herubiolog@gmail.com>
2022-12-27 15:53:47 -0600

    Move watcher to lower widget tree.

--- lib/screens/narrative/narrative_view.dart
+++ lib/screens/narrative/narrative_view.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nahpu/models/catalogs.dart';
 import 'package:nahpu/models/form.dart';
-
 import 'package:nahpu/providers/catalogs.dart';
-
 import 'package:nahpu/screens/narrative/components/menu_bar.dart';
 import 'package:nahpu/screens/narrative/narrative_form.dart';
 import 'package:nahpu/screens/shared/buttons.dart';
@@ -32,8 +30,6 @@ class NarrativeState extends ConsumerState<Narrative> {
 
   @override
   Widget build(BuildContext context) {
-    final narrativeEntries = ref.watch(narrativeEntryProvider);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Narrative"),
@@ -46,50 +42,51 @@ class NarrativeState extends ConsumerState<Narrative> {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Center(
-          child: narrativeEntries.when(
-            data: (narrativeEntries) {
-              if (narrativeEntries.isEmpty) {
-                setState(() {
-                  isVisible = false;
-                });
+          child: ref.watch(narrativeEntryProvider).when(
+                data: (narrativeEntries) {
+                  if (narrativeEntries.isEmpty) {
+                    setState(() {
+                      isVisible = false;
+                    });
 
-                return const Text("No narrative entries");
-              } else {
-                int narrativeSize = narrativeEntries.length;
-                setState(() {
-                  if (narrativeSize >= 2) {
-                    isVisible = true;
-                  }
-                  _pageNav.pageCounts = narrativeSize;
+                    return const Text("No narrative entries");
+                  } else {
+                    int narrativeSize = narrativeEntries.length;
+                    setState(() {
+                      if (narrativeSize >= 2) {
+                        isVisible = true;
+                      }
+                      _pageNav.pageCounts = narrativeSize;
 
-                  // We want to view the last page first.
-                  // Dart uses 0-based indexing. Technically, this is out-of-bound.
-                  // But, what happens here is that it will trigger the PageView onPageChanged.
-                  // It fixes the issues that the curentPage state does not show the current page value.
-                  pageController = PageController(initialPage: narrativeSize);
-                });
-                return PageView.builder(
-                  controller: pageController,
-                  itemCount: narrativeSize,
-                  itemBuilder: (context, index) {
-                    final narrativeCtr =
-                        _updateController(narrativeEntries, index);
-                    return NarrativeForm(
-                      narrativeId: narrativeEntries[index].id,
-                      narrativeCtr: narrativeCtr,
+                      // We want to view the last page first.
+                      // Dart uses 0-based indexing. Technically, this is out-of-bound.
+                      // But, what happens here is that it will trigger the PageView onPageChanged.
+                      // It fixes the issues that the curentPage state does not show the current page value.
+                      pageController =
+                          PageController(initialPage: narrativeSize);
+                    });
+                    return PageView.builder(
+                      controller: pageController,
+                      itemCount: narrativeSize,
+                      itemBuilder: (context, index) {
+                        final narrativeCtr =
+                            _updateController(narrativeEntries, index);
+                        return NarrativeForm(
+                          narrativeId: narrativeEntries[index].id,
+                          narrativeCtr: narrativeCtr,
+                        );
+                      },
+                      onPageChanged: (value) => setState(() {
+                        _pageNav.currentPage = value + 1;
+                        _pageNav = updatePageNavigation(_pageNav);
+                        ref.invalidate(narrativeEntryProvider);
+                      }),
                     );
-                  },
-                  onPageChanged: (value) => setState(() {
-                    _pageNav.currentPage = value + 1;
-                    _pageNav = updatePageNavigation(_pageNav);
-                    ref.invalidate(narrativeEntryProvider);
-                  }),
-                );
-              }
-            },
-            loading: () => const CommmonProgressIndicator(),
-            error: (error, stack) => Text(error.toString()),
-          ),
+                  }
+                },
+                loading: () => const CommmonProgressIndicator(),
+                error: (error, stack) => Text(error.toString()),
+              ),
         ),
       ),
       bottomSheet: Visibility(
