8a69e4e762857ff2958d125f39a22e11b91e27f9
Heru Handika <herubiolog@gmail.com>
2023-05-13 14:48:35 -0500

    Remove old windows build script.

--- scripts/build_windows.ps1
+++
@@ -1,6 +0,0 @@
-# Description: Build Windows release and copy to portable program folder
-Write-Output "Building Windows release"
-flutter build windows --release
-Write-Output "Copying to portable program folder"
-Copy-Item -Recurse -Force -Path '.\build\windows\runner\Release\' -Destination 'C:\Users\hhandika\Documents\PortableProgram\nahpu'
-Start-Process 'C:\Users\hhandika\Documents\PortableProgram\nahpu\nahpu.exe'
