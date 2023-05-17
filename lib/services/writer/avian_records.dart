489f69035a7536788b5ec01da97f2d549c37c3f5
Heru Handika <herubiolog@gmail.com>
2023-05-17 16:36:42 -0500

    Update bird measurements.

--- lib/services/writer/avian_records.dart
+++ lib/services/writer/avian_records.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nahpu/models/types.dart';
 import 'package:nahpu/services/database/database.dart';
 import 'package:nahpu/services/specimen_services.dart';
 
@@ -16,11 +17,46 @@ class AvianMeasurements {
 
   Future<String> getMeasurements() async {
     data = await SpecimenServices(ref).getAvianMeasurementData(specimenUuid);
-    String weight = getWeight();
-    return weight;
+    String weight = _getWeight();
+    String wingSpan = _getWingSpan();
+    String irisColor = _getIrisColor();
+    String billColor = _getBillColor();
+    String footColor = _getFootColor();
+    String tarsusColor = _getTarsusColor();
+    String sexData = _getSexData();
+    return '$sexData$weight$delimiter$wingSpan$delimiter$irisColor$delimiter'
+        '$billColor$delimiter$footColor$delimiter$tarsusColor';
   }
 
-  String getWeight() {
+  String _getWeight() {
     return '${data.weight ?? ''}';
+  }
+
+  String _getWingSpan() {
+    return '${data.wingspan ?? ''}';
+  }
+
+  String _getIrisColor() {
+    return data.irisColor ?? '';
+  }
+
+  String _getBillColor() {
+    return data.billColor ?? '';
+  }
+
+  String _getFootColor() {
+    return data.footColor ?? '';
+  }
+
+  String _getTarsusColor() {
+    return data.tarsusColor ?? '';
+  }
+
+  String _getSexData() {
+    SpecimenSex? sexEnum = getSpecimenSex(data.sex);
+    String sex = data.sex != null ? specimenSexList[data.sex!] : '';
+    String emptyMale = delimiter;
+    String emptyFemale = delimiter * 2;
+    return '$sex$delimiter$emptyMale$emptyFemale';
   }
 }
