ed40895f7a7d2d84d7ef463a376878e2b1ba2459
Heru Handika <herubiolog@gmail.com>
2023-05-17 12:59:21 -0500

    Make scrollbar always visible for specimen measurement forms.

--- lib/screens/specimens/shared/measurements.dart
+++ lib/screens/specimens/shared/measurements.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:nahpu/screens/shared/forms.dart';
 
-class MeasurementForm extends StatelessWidget {
-  const MeasurementForm({super.key, required this.children});
+class MeasurementForm extends StatefulWidget {
+  const MeasurementForm({
+    super.key,
+    required this.children,
+  });
 
   final List<Widget> children;
+
+  @override
+  State<MeasurementForm> createState() => _MeasurementFormState();
+}
+
+class _MeasurementFormState extends State<MeasurementForm> {
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +24,15 @@ class MeasurementForm extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       child: SizedBox(
         height: MediaQuery.of(context).size.height * 0.5,
-        child: SingleChildScrollView(
-          child: Column(
-            children: children,
+        child: Scrollbar(
+          thumbVisibility: true,
+          controller: _scrollController,
+          thickness: 5,
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              children: widget.children,
+            ),
           ),
         ),
       ),
