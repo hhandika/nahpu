1cc5493a996ed538822ff26ca7c88178d0114baf
Heru Handika <herubiolog@gmail.com>
2023-05-20 16:30:06 -0500

    Use shared auto complete.

--- lib/screens/specimens/shared/taxa.dart
+++ lib/screens/specimens/shared/taxa.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:nahpu/screens/shared/fields.dart';
 import 'package:nahpu/services/database/database.dart';
 import 'package:nahpu/services/specimen_services.dart';
 import 'package:nahpu/providers/catalogs.dart';
@@ -25,72 +26,20 @@ class SpeciesAutoComplete extends ConsumerStatefulWidget {
 }
 
 class SpeciesAutoCompleteState extends ConsumerState<SpeciesAutoComplete> {
-  FocusNode _focusNode = FocusNode();
-
+  final FocusNode _focusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
-    return RawAutocomplete<String>(
+    return AutoCompleteField(
       focusNode: _focusNode,
-      textEditingController: widget.speciesCtr,
-      optionsBuilder: (TextEditingValue textEditingValue) {
-        if (textEditingValue.text == '') {
-          return const Iterable<String>.empty();
-        }
-        return widget.options.where((String option) {
-          return option
-              .toLowerCase()
-              .contains(textEditingValue.text.toLowerCase());
-        });
-      },
+      controller: widget.speciesCtr,
+      options: widget.options,
+      labelText: 'Species',
+      hintText: 'Enter species name',
       onSelected: (String selection) {
         setState(() {
           _inputTaxon(selection);
         });
         _focusNode.unfocus();
-      },
-      fieldViewBuilder: (
-        BuildContext context,
-        TextEditingController controller,
-        FocusNode focusNode,
-        VoidCallback onFieldSubmitted,
-      ) {
-        return SpeciesField(
-          speciesCtr: controller,
-          enable: true,
-          focusNode: focusNode,
-          onFieldSubmitted: (String value) {
-            onFieldSubmitted();
-          },
-        );
-      },
-      optionsViewBuilder: (BuildContext context,
-          AutocompleteOnSelected<String> onSelected, Iterable<String> options) {
-        return Align(
-          alignment: Alignment.topLeft,
-          child: Material(
-            elevation: 4.0,
-            child: Container(
-              width: 300,
-              constraints: const BoxConstraints(maxHeight: 300),
-              child: ListView.builder(
-                shrinkWrap: true,
-                padding: const EdgeInsets.all(8.0),
-                itemCount: options.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final String option = options.elementAt(index);
-                  return GestureDetector(
-                    onTap: () {
-                      onSelected(option);
-                    },
-                    child: ListTile(
-                      title: Text(option),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        );
       },
     );
   }
