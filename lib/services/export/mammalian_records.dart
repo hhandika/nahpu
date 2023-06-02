d6f21456d1fb2b9ee8e4e246c9f1484ee8f888a7
Heru Handika <herubiolog@gmail.com>
2023-06-01 20:56:07 -0500

    Fix mammal spreadsheet does not align with headers.

--- lib/services/export/mammalian_records.dart
+++ lib/services/export/mammalian_records.dart
@@ -21,10 +21,7 @@ class MammalianMeasurements {
   Future<String> getMeasurements() async {
     data =
         await SpecimenServices(ref: ref).getMammalMeasurementData(specimenUuid);
-    String standardMeasurement =
-        '${data.totalLength ?? ''}$delimiter${data.tailLength ?? ''}$delimiter'
-        '${data.hindFootLength ?? ''}$delimiter${data.earLength ?? ''}$delimiter'
-        '${data.weight ?? ''}$delimiter${data.accuracy ?? ''}';
+    String standardMeasurement = _getStdMeasurement();
     String measurement = isBatRecord
         ? '$standardMeasurement$delimiter${data.forearm ?? ''}'
         : standardMeasurement;
@@ -33,18 +30,36 @@ class MammalianMeasurements {
     return '$measurement$delimiter$age$delimiter$sexData';
   }
 
+  String _getStdMeasurement() {
+    String totalLength = '${data.totalLength ?? ''}';
+    String tailLength = '${data.tailLength ?? ''}';
+    String hindFootLength = '${data.hindFootLength ?? ''}';
+    String earLength = '${data.earLength ?? ''}';
+    String weight = '${data.weight ?? ''}';
+    String accuracy = data.accuracy ?? '';
+    List<String> measurementList = [
+      totalLength,
+      tailLength,
+      hindFootLength,
+      earLength,
+      weight,
+      accuracy,
+    ];
+    return measurementList.join(delimiter);
+  }
+
   String _getSexData() {
     SpecimenSex? sexEnum = getSpecimenSex(data.sex);
     String sex = data.sex != null ? specimenSexList[data.sex!] : '';
-    String emptyMale = delimiter;
-    String emptyFemale = delimiter * 2;
+    String emptyMale = delimiter * 2;
+    String emptyFemale = delimiter * 3;
     switch (sexEnum) {
       case SpecimenSex.male:
         String maleGonad = _getMaleGonad();
         return '$sex$delimiter$maleGonad$emptyFemale';
       case SpecimenSex.female:
         String femaleGonad = _getFemaleGonad();
-        return '$sex$delimiter$emptyMale$delimiter$femaleGonad';
+        return '$sex$delimiter$emptyMale$femaleGonad';
       case SpecimenSex.unknown:
         return '$sex$delimiter$emptyMale$emptyFemale';
       default:
@@ -58,21 +73,26 @@ class MammalianMeasurements {
         : '';
     if (data.mammaeCondition != null) {
       String mammaeCondition = mammaeConditionList[data.mammaeCondition!];
-      String ingCount = data.mammaeInguinalCount != null
-          ? '${data.mammaeInguinalCount} ing;'
-          : '';
-      String abdCount = data.mammaeAbdominalCount != null
-          ? '${data.mammaeAbdominalCount} abd;'
-          : '';
-      String axCount = data.mammaeAxillaryCount != null
-          ? '${data.mammaeAxillaryCount} ax'
-          : '';
-      String mammaeCount = '$ingCount$abdCount$axCount';
-      return '$vaginaOpening$delimiter$mammaeCondition$delimiter$mammaeCount';
+      String mammaeFormula = _getMammaeFormula();
+      return '$vaginaOpening$delimiter$mammaeCondition$delimiter$mammaeFormula';
     } else {
       String empty = delimiter * 2;
       return '$vaginaOpening$empty';
     }
+  }
+
+  String _getMammaeFormula() {
+    String ingCount = data.mammaeInguinalCount != null
+        ? '${data.mammaeInguinalCount} ing;'
+        : '';
+    String abdCount = data.mammaeAbdominalCount != null
+        ? '${data.mammaeAbdominalCount} abd;'
+        : '';
+    String axCount = data.mammaeAxillaryCount != null
+        ? '${data.mammaeAxillaryCount} ax'
+        : '';
+
+    return '$ingCount$abdCount$axCount';
   }
 
   String _matchTestisPos(int? testisPos) {
