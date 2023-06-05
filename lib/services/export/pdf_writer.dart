bf811b530013b8d5de99a097607766e53e013ef9
Heru Handika <herubiolog@gmail.com>
2023-06-05 17:41:30 -0500

    Fix export when data is empty

--- lib/services/export/pdf_writer.dart
+++ lib/services/export/pdf_writer.dart
@@ -1,5 +1,7 @@
 import 'package:nahpu/services/database/database.dart';
+import 'package:nahpu/services/export/site_writer.dart';
 import 'package:nahpu/services/io_services.dart';
+import 'package:nahpu/services/narrative_services.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
@@ -13,24 +15,36 @@ class NarrativePdfWriter extends DbAccess {
 
   Future<pw.Document> generatePdf() async {
     final pdf = pw.Document();
+    List<NarrativeData> narrativeList =
+        await NarrativeServices(ref: ref).getAllNarrative();
+    for (var narrative in narrativeList) {
+      await generateNarrativePage(pdf, narrative);
+    }
 
     return pdf;
   }
 
-  Future<pw.Document> generateNarrativePage(
+  Future<void> generateNarrativePage(
       pw.Document pdf, NarrativeData data) async {
+    String verbatimLocality =
+        await SiteWriterServices(ref: ref).getVerbatimLocality(data.siteID);
     pdf.addPage(
       pw.Page(
         pageFormat: pageFormat,
         build: (pw.Context context) {
           return pw.Column(children: [
             pw.Container(
-              decoration: pw.BoxDecoration(border: pw.Border.all()),
-              child: pw.Text(
-                data.date ?? 'No date',
-                style: const pw.TextStyle(fontSize: 14),
-              ),
-            ),
+                decoration: pw.BoxDecoration(border: pw.Border.all()),
+                child: pw.Column(children: [
+                  pw.Text(
+                    data.date ?? 'No date',
+                    style: const pw.TextStyle(fontSize: 14),
+                  ),
+                  pw.Text(
+                    verbatimLocality,
+                    style: const pw.TextStyle(fontSize: 14),
+                  ),
+                ])),
             pw.SizedBox(height: 16),
             pw.Container(
               decoration: pw.BoxDecoration(border: pw.Border.all()),
@@ -43,6 +57,5 @@ class NarrativePdfWriter extends DbAccess {
         },
       ),
     );
-    return pdf;
   }
 }
