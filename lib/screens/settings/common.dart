b1b604c46e0e04bab9991f1a9f002dee9a223eae
Heru Handika <herubiolog@gmail.com>
2023-07-29 23:00:44 -0500

    Update colors.

--- lib/screens/settings/common.dart
+++ lib/screens/settings/common.dart
@@ -299,7 +299,7 @@ class CommonChip extends StatelessWidget {
       backgroundColor: Color.lerp(
         primaryColor,
         Theme.of(context).colorScheme.surface,
-        0.75,
+        0.85,
       ),
       onDeleted: onDeleted,
     );
