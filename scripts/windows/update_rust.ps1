23d5bb1b15d1f854e0f71cc54344e4c45bd28fef
Heru Handika <herubiolog@gmail.com>
2024-02-27 12:29:48 -0600

    Update windows scripts.

---
+++ scripts/windows/update_rust.ps1
@@ -0,0 +1,5 @@
+cd rust/
+cargo install 'flutter_rust_bridge_codegen@^2.0.0-dev.0'
+
+cargo update
+cargo check
---
+++ scripts/windows/windows_setup.ps1
@@ -0,0 +1,4 @@
+winget install -e --id LLVM.LLVM
+cargo install flutter_rust_bridge_codegen
+cargo install cargo-ndk
+rustup target add aarch64-linux-android armv7-linux-androideabi x86_64-linux-android i686-linux-android
