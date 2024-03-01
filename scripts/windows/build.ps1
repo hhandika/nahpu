bbe01775738564022ac0187971a0d4f7dbd8af5a
Heru Handika <herubiolog@gmail.com>
2024-03-01 14:05:03 -0600

    Clean and change destination file to avoid conflict with the app doc file.

--- scripts/windows/build.ps1
+++ scripts/windows/build.ps1
@@ -1,29 +1,20 @@
 flutter build windows --release
 
-# Build path
-$target = "Release"
-$buildExe = "build\windows\x64\runner\Release\nahpu.exe"
-$buildPath = "build\windows\x64\runner\$target"
-$releasePath = "$env:USERPROFILE\Documents\"
-$exePath = "$env:USERPROFILE\Documents\nahpu\nahpu.exe"
+$buildPath = "build\windows\x64\runner\Release"
+$buildExe = "$buildPath\nahpu.exe"
+$destinationPath = "$env:USERPROFILE\Documents\nahpu-app"
+$exePath = "$env:USERPROFILE\Documents\nahpu-app\nahpu.exe"
 $archivePath = "$env:USERPROFILE\Documents\Releases\"
-$appName = "nahpu"
 # Check build file exists
 if (Test-Path -Path $buildExe) {
     Write-Host "Build succeeded"
     # Copy relese files to user document folder
-    
-    if (Test-Path -Path $releasePath\$appName) {
-        Remove-Item -Path $releasePath\$appName -Recurse -Force
+    if (Test-Path -Path $destinationPath) {
+        Remove-Item -Path $destinationPath -Recurse -Force
     }
-    Copy-Item -Path $buildPath -Destination $releasePath -Recurse
-
-    Write-Host "Rename $deployPath to $appName"
-    Rename-Item  $releasePath\$target $appName
-
-    Write-Host "Release files copied to $releasePath"
-    New-Item -ItemType Directory -Path $archivePath -Force
-    Compress-Archive -Path $releasePath\$appName -DestinationPath $archivePath\$appName-windows.zip -Force
+    Copy-Item -Path $buildPath -Destination $destinationPath -Recurse
+    Write-Host "Release files copied to $destinationPath"
+    
     Start-Process $exePath
 } else {
     Write-Host "Build failed"
