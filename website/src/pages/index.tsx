3c84d00c2f0f6420a40b9fd3a044302ff7c6ddd1
Heru Handika <herubiolog@gmail.com>
2024-03-12 11:51:04 -0500

    Avoid conflict with npm

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--primary button--lg" to="/docs/intro">
+          <Link className="button button--primary button--lg" to="/docs/index">
             Learn NAHPU
           </Link>
         </div>
