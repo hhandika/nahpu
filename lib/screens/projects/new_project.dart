7e38bc09153b1992d925c9d09250e5139c27dad4
Heru Handika <herubiolog@gmail.com>
2024-03-21 16:55:08 -0500

    Add UUID and clean scanner code.

--- lib/screens/projects/new_project.dart
+++ lib/screens/projects/new_project.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:nahpu/screens/shared/forms.dart';
+import 'package:nahpu/screens/shared/qr.dart';
 import 'package:nahpu/services/database/database.dart';
 import 'package:nahpu/services/platform_services.dart';
 import 'package:nahpu/services/providers/validation.dart';
@@ -22,6 +22,7 @@ class CreateProjectForm extends ConsumerStatefulWidget {
 class CreateProjectFormState extends ConsumerState<CreateProjectForm> {
   String _uuidKey = uuid;
   final ProjectFormCtrModel projectCtr = ProjectFormCtrModel.empty();
+  bool _qrScanned = false;
 
   @override
   void initState() {
@@ -48,63 +49,8 @@ class CreateProjectFormState extends ConsumerState<CreateProjectForm> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               if (systemPlatform == PlatformType.mobile)
-                GestureDetector(
-                  child: Container(
-                    width: 180,
-                    height: 180,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context)
-                          .colorScheme
-                          .primaryContainer
-                          .withAlpha(80),
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    padding: const EdgeInsets.all(4),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const QrIcon(),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8),
-                          child: RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: 'Scan QR',
-                                  style: Theme.of(context).textTheme.titleLarge,
-                                ),
-                                const WidgetSpan(
-                                    child: InfoButton(
-                                  content: Text(
-                                    'Scan QR code from other projects to import data. '
-                                    'This method is useful when multiple devices are '
-                                    'used to manage the same project. '
-                                    'To get the QR code, go to the project dashboard '
-                                    'in the other device. '
-                                    'Scan the QR code in the project overview. '
-                                    'You can also tap the QR code to enlarge it.',
-                                  ),
-                                ))
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ScannerScreen(
-                          onDetect: (BarcodeCapture barcode) {
-                            onDetect(barcode);
-                          },
-                        ),
-                      ),
-                    );
-                  },
-                ),
+                QrCaptureButton(onDetect: onDetect),
+              if (_qrScanned) UuidText(uuid: _uuidKey),
               project.ProjectForm(
                 projectCtr: projectCtr,
                 projectUuid: _uuidKey,
@@ -141,7 +87,8 @@ class CreateProjectFormState extends ConsumerState<CreateProjectForm> {
         .checkProjectNameExists(projectData.name);
     setState(() {
       projectCtr.updateData(projectData);
-      _uuidKey = projectData.uuid;
+      _uuidKey = uuid;
+      _qrScanned = true;
     });
   }
 
@@ -177,62 +124,94 @@ class CreateProjectFormState extends ConsumerState<CreateProjectForm> {
   }
 }
 
-class ScannerScreen extends StatefulWidget {
-  const ScannerScreen({super.key, required this.onDetect});
+class UuidText extends StatelessWidget {
+  const UuidText({super.key, required this.uuid});
+
+  final String uuid;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: RichText(
+            text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'UUID: ',
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+            TextSpan(
+              text: uuid,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        )));
+  }
+}
+
+class QrCaptureButton extends StatelessWidget {
+  const QrCaptureButton({
+    super.key,
+    required this.onDetect,
+  });
 
   final void Function(BarcodeCapture) onDetect;
 
   @override
-  State<ScannerScreen> createState() => _ScannerScreenState();
-}
-
-class _ScannerScreenState extends State<ScannerScreen> {
-  final MobileScannerController _controller = MobileScannerController(
-    detectionSpeed: DetectionSpeed.normal,
-    facing: CameraFacing.back,
-  );
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.start();
-  }
-
-  @override
-  void dispose() {
-    _controller.stop();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Scan QR code'),
+    return GestureDetector(
+      child: Container(
+        width: 180,
+        height: 180,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primaryContainer.withAlpha(80),
+          borderRadius: BorderRadius.circular(16),
         ),
-        body: MobileScanner(
-          controller: _controller,
-          onDetect: (barcode) {
-            widget.onDetect(barcode);
-            _controller.stop();
-          },
-        ));
-  }
-}
-
-class QrIcon extends StatelessWidget {
-  const QrIcon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SvgPicture.asset(
-      'assets/icons/qr-code.svg',
-      width: 100,
-      height: 100,
-      colorFilter: ColorFilter.mode(
-        Theme.of(context).colorScheme.onSurface,
-        BlendMode.srcIn,
+        padding: const EdgeInsets.all(4),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const QrIcon(),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Scan QR',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    const WidgetSpan(
+                        child: InfoButton(
+                      content: Text(
+                        'Scan QR code from other projects to import data. '
+                        'This method is useful when multiple devices are '
+                        'used to manage the same project. '
+                        'To get the QR code, go to the project dashboard '
+                        'in the other device. '
+                        'Scan the QR code in the project overview. '
+                        'You can also tap the QR code to enlarge it.',
+                      ),
+                    ))
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ScannerScreen(
+              onDetect: (BarcodeCapture barcode) {
+                onDetect(barcode);
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/screens/shared/qr.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ScannerScreen extends StatefulWidget {
+  const ScannerScreen({super.key, required this.onDetect});
+
+  final void Function(BarcodeCapture) onDetect;
+
+  @override
+  State<ScannerScreen> createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  final MobileScannerController _controller = MobileScannerController(
+    detectionSpeed: DetectionSpeed.normal,
+    facing: CameraFacing.back,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.start();
+  }
+
+  @override
+  void dispose() {
+    _controller.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Scan QR code'),
+        ),
+        body: MobileScanner(
+          controller: _controller,
+          onDetect: (barcode) {
+            widget.onDetect(barcode);
+            _controller.stop();
+          },
+        ));
+  }
+}
+
+class QrIcon extends StatelessWidget {
+  const QrIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      'assets/icons/qr-code.svg',
+      width: 100,
+      height: 100,
+      colorFilter: ColorFilter.mode(
+        Theme.of(context).colorScheme.onSurface,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+}
