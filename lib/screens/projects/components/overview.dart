f99f1cec5d0f1eeec40ae1c655261a04243d0a0e
Heru Handika <herubiolog@gmail.com>
2024-03-21 18:45:32 -0500

    Add temp info. Future info should be more concise with link to details.

--- lib/screens/projects/components/overview.dart
+++ lib/screens/projects/components/overview.dart
@@ -56,20 +56,34 @@ class ProjectInfoContent extends StatelessWidget {
           header: 'Overview',
           content: 'Basic information about the project.'
               ' You can edit the project information'
-              ' by clicking the edit button in the bottom right corner.',
-        ),
-        InfoContent(
-          content: 'Keep the description short and concise.'
-              ' Provide only general info about the location,'
-              ' e.g. Mt. Gede, Java, Indonesia.',
+              ' by clicking the edit button in the bottom right corner.'
+              ' To share the project details with others,'
+              ' use the QR code.',
         ),
         InfoContent(
           header: 'UUID',
-          content: 'Unique identifier for the project.'
-              ' UUID is generated automatically when you create a new project.'
-              ' It ensure that each project is unique'
-              ' to avoid data collision in the future.'
-              ' UUID is used to identify the project in the database.',
+          content: 'UUID is a universal unique identifier.'
+              ' It is automatically generated when you create a new project.'
+              ' It standardizes the project identification process,'
+              ' making it easy to find, manage, and share project data.',
+        ),
+        InfoContent(
+          header: 'Sharing project details',
+          content: 'Current version only supports sharing project details '
+              'via QR code and only available to import using mobile devices. Tap the QR code to view it in full. '
+              'On other devices, create a new project and use the '
+              '"Scan QR" button to scan the QR code from this project. '
+              'The QR code contains essential project information, '
+              'making it convenient to consolidate data when working '
+              'with multiple devices on the same project.',
+        ),
+        InfoContent(
+          header: 'Tips',
+          content: 'Keep the description short and concise.'
+              ' Provide only general information about the location,'
+              ' e.g. Mt. Gede, Java, Indonesia.'
+              ' We recommend using the narrative to provide'
+              ' more detailed information about the project.',
         ),
       ],
     );
