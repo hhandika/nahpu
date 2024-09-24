d2fc3efe8dd5639ea3e9be08ac36aaa56b02ad33
Heru Handika <herubiolog@gmail.com>
2024-09-24 11:56:34 -0500

    Add dummy test.

--- integration_test/simple_test.dart
+++ integration_test/simple_test.dart
@@ -1,4 +1,8 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:nahpu/main.dart';
-// import 'package:nahpu/src/rust/frb_generated.dart';
-// import 'package:integration_test/integration_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nahpu/src/rust/frb_generated.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  setUpAll(() async => await RustLib.init());
+}
